c65d2c5d9002bed6db7894fdb69762ec9b114024
keylor2002 <keylor.barboza@ucrso.info>
2024-06-01 15:52:33 -0600

    Add roles & Controllers

--- Controllers/UserController/ControladorLogin.cs
+++ Controllers/UserController/ControladorLogin.cs
@@ -1,6 +1,37 @@
-﻿namespace StarFood.Controllers.User_Controller
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StarFood.Repository.IRepository;
+
+namespace StarFood.Controllers.User_Controller
 {
-    public class ControladorLogin
+    public class ControladorLogin : Controller
     {
+        private IUnitOfWork _db;
+        private UserManager<IdentityUser> _userManager;
+
+        public ControladorLogin(IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager)
+        {
+            _db = unitOfWork;
+            _userManager = userManager;
+        }
+
+        //API 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public IActionResult Login(string nombreUsuario, string contraseña)
+        {
+            IdentityUser user = _userManager.FindByNameAsync(nombreUsuario).Result;
+            if (user != null && _userManager.CheckPasswordAsync(user, contraseña).Result)
+            {
+                return Json(new { data = user.Id, success = true });
+            }
+            else
+            {
+                return Json(new { data = "No se pudo encontrar el usuario", success = false });
+            }
+        }
     }
 }
--- Controllers/UserController/ControladorUsuario.cs
+++ Controllers/UserController/ControladorUsuario.cs
@@ -1,6 +1,128 @@
-﻿namespace StarFood.Controllers.User_Controller
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using StarFood.Repository.IRepository;
+using StarFood.Utility;
+
+namespace StarFood.Controllers.User_Controller
 {
-    public class ControladorUsuario
+    [Authorize(Roles = Roles.Admin_Role + "," + Roles.Empleado_Role)]
+    public class ControladorUsuario : Controller
     {
+        private IUnitOfWork _db;
+        private UserManager<IdentityUser> _userManager;
+
+        public ControladorUsuario(IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager)
+        {
+            _db = unitOfWork;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _db.Usuario.GetAll();
+            if (User.IsInRole(Roles.Admin_Role))
+            {
+                IList<IdentityUser> usersAux = _userManager.GetUsersInRoleAsync(Roles.Empleado_Role).Result;
+                List<Models.Usuario> usersList = new List<Models.Usuario>();
+                foreach (var user in usersAux)
+                {
+                    usersList.Add(_db.Usuario.GetFirstOrDefault(x => x.Id == user.Id, null));
+                }
+            }
+            return View(users);
+        }
+
+        public IActionResult Edit(string? id)   //Update + Insert
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = _db.Usuario.GetFirstOrDefault(x => x.Id == id, null);
+            //var categoryFromDb = _db.Categories.FirstOrDefault(x => x.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Models.Usuario obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Usuario.Update(obj);
+                _db.Save();
+                TempData["success"] = "User Updated successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Attend(Models.Usuario obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Usuario.Update(obj);
+                _db.Save();
+                TempData["success"] = "User Updated successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult Banned(string? id)
+        {
+            if (id != null)
+            {
+                _userManager.SetLockoutEnabledAsync(_db.Usuario.GetFirstOrDefault(x => x.Id == id, null), true);
+                _userManager.SetLockoutEndDateAsync(_db.Usuario.GetFirstOrDefault(x => x.Id == id, null), DateTimeOffset.MaxValue);
+            }
+            _db.Save();
+            return Json(new { success = true, message = "Banned Successfully" });
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult Unbanned(string? id)
+        {
+            if (id != null)
+            {
+                _userManager.SetLockoutEnabledAsync(_db.Usuario.GetFirstOrDefault(x => x.Id == id, null), false);
+                _userManager.SetLockoutEndDateAsync(_db.Usuario.GetFirstOrDefault(x => x.Id == id, null), null);
+            }
+            _db.Save();
+            return Json(new { success = true, message = "Unbanned Successfully" });
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _db.Usuario.GetAll();
+            return Json(new { data = users, success = true });
+        }
+
+        [HttpGet]
+        public IActionResult GetAllMedical()
+        {
+            IList<IdentityUser> users = _userManager.GetUsersInRoleAsync(Roles.Empleado_Role).Result;
+            List<Models.Usuario> usersList = new List<Models.Usuario>();
+            foreach (var user in users)
+            {
+                usersList.Add(_db.Usuario.GetFirstOrDefault(x => x.Id == user.Id, null));
+            }
+            return Json(new { data = usersList, success = true });
+        }
+
+    
     }
 }
