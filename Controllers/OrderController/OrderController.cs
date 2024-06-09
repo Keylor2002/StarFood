2915acf9b7e28d2f15342b74f3c4d44ae10b1008
keylor2002 <keylor.barboza@ucrso.info>
2024-06-09 11:44:12 -0600

    Add repository detail order

---
+++ Controllers/OrderController/OrderController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarFood.Models;
+using StarFood.Repository.IRepository;
+
+namespace StarFood.Controllers.OrderController
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private IWebHostEnvironment _webHostEnvironment;
+
+        public OrderController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        public IActionResult Index()
+        {
+            //IEnumerable<Categoria> categoryList = _unitOfWork.Categoria.GetAll();
+            //return View(categoryList);
+            return View();
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Works
+        [HttpPost]
+        public IActionResult Create([FromBody] Pedido order)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Pedido.Add(order);
+                _unitOfWork.Save();
+                //return Json(new { success = true, message = "Categoria creada correctamente" });
+            }
+            TempData["success"] = "Nuevo pedido realizado";
+            return RedirectToAction("Index");
+            //return Json(new { success = false, errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage) });
+        }
+
+
+        // Works
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound(new { success = false, message = "ID no proporcionado" });
+            }
+
+            var order = _unitOfWork.Pedido.GetFirstOrDefault(x => x.IDPedido == id, null);
+            if (order == null)
+            {
+                return NotFound(new { success = false, message = "Pedido no encontrado" });
+            }
+
+            return Json(new { success = true, data = order });
+        }
+
+
+        // Works
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Edit([FromBody] Pedido order)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Pedido.Update(order);
+                _unitOfWork.Save();
+                //return Json(new { success = true, message = "Categoria actualizada correctamente" });
+            }
+            TempData["success"] = "Pedido editado correctamente";
+            return RedirectToAction("Index");
+            //return Json(new { success = false, errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage) });
+        }
+
+        //[HttpDelete]
+        //public IActionResult Delete(int? id)
+        //{
+        //    var CategoryToDelete = _unitOfWork.Categoria.Get(u => u.IDCategoria == id);
+
+        //    if (CategoryToDelete == null)
+        //    {
+        //        return Json(new { success = false, message = "Error while deleting category" });
+        //    }
+
+        //    _unitOfWork.Categoria.Remove(CategoryToDelete);
+        //    _unitOfWork.Save();
+        //    return Json(new { success = true, message = "Category deleted successfully" });
+        //}
+
+        //public IActionResult Suspend(int? id)
+        //{
+
+        //    var category = _unitOfWork.Categoria.GetFirstOrDefault(x => x.IDCategoria == id, null);
+
+        //    if (category.Suspendido == false)
+        //    {
+        //        category.Suspendido = true;
+        //        TempData["success"] = "Categoria suspendida correctamente";
+        //    }
+        //    else
+        //    {
+        //        category.Suspendida = false;
+        //        TempData["success"] = "La categoria se activo correctamente";
+        //    }
+        //    _unitOfWork.Categoria.Update(category);
+        //    _unitOfWork.Save();
+        //    return RedirectToAction("Index");
+        //}
+
+        // Works
+        public IActionResult GetAll()
+        {
+            var order = _unitOfWork.Pedido.GetAll();
+            return Json(new { data = order, success = true });
+        }
+    }
+}
---
+++ Controllers/PaymentMethodController/PaymentMethodController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StarFood.Models;
+using StarFood.Repository.IRepository;
+
+namespace StarFood.Controllers.PaymentMethodController
+{
+    public class PaymentMethodController : Controller
+    {
+       
+        private readonly IUnitOfWork _unitOfWork;
+        private IWebHostEnvironment _hostEnvironment;
+        // GET: HomeController
+
+        public PaymentMethodController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<MetodoPago> paymentMethodList = _unitOfWork.MetodoPago.GetAll();
+            return View(paymentMethodList);
+        }
+
+        // GET: HomeController/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeController/Create
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Create([FromBody] MetodoPago paymentMethod)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.MetodoPago.Add(paymentMethod);
+                _unitOfWork.Save();
+            }
+            TempData["success"] = "Metodo de pago agregado correctamente";
+            return RedirectToAction("Index");
+        }
+
+        // GET: HomeController/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = _unitOfWork.MetodoPago.GetFirstOrDefault(x => x.IDMetodoPago == id, null);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: HomeController/Edit/5
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Edit([FromBody] MetodoPago paymentMethod)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.MetodoPago.Update(paymentMethod);
+                _unitOfWork.Save();
+            }
+            TempData["success"] = "Metodo de pago editado correctamente";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult GetAll()
+        {
+            var paymentMethod = _unitOfWork.MetodoPago.GetAll();
+            return Json(new { data = paymentMethod, success = true });
+        }
+
+        //// GET: HomeController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: HomeController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
