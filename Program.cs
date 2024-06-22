66b70995af13ae54fca50513b682e9c6e13ecf20
keylor2002 <keylor.barboza@ucrso.info>
2024-06-21 19:23:21 -0600

    Pull me

--- Program.cs
+++ Program.cs
@@ -77,6 +77,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}");
+    pattern: "{controller=Home}/{action=Index}/{id=?}"
+    );
 
 app.Run();
